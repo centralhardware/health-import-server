ac10dfd13808767541ed856bbc2d70d824d81973
alex <alex@centralhardware.me>
2025-06-05 09:56:24 +0800

    fix migration

--- src/main/resources/migration/V2__remove_qty_from_heart_rate.sql
+++ src/main/resources/migration/V2__remove_qty_from_heart_rate.sql
@@ -1,2 +1,2 @@
 ALTER TABLE ${database}.workout_heart_rate_data DROP COLUMN IF EXISTS qty;
-ALTER TABLE ${database}.workout_heart_rate_data_recovery DROP COLUMN IF EXISTS qty
+ALTER TABLE ${database}.workout_heart_rate_recovery DROP COLUMN IF EXISTS qty
